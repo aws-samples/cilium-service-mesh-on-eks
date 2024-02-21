c892b4a56d2a0dc6423679047574d566c5f030b8
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-02-21 22:15:30 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,9 +37,6 @@ locals {
   vpc_cidr = "10.0.0.0/16"
   azs      = slice(data.aws_availability_zones.available.names, 0, 3)
 
-  istio_chart_url     = "https://istio-release.storage.googleapis.com/charts"
-  istio_chart_version = "1.20.2"
-
   tags = {
     Blueprint  = local.name
     GithubRepo = "github.com/aws-ia/terraform-aws-eks-blueprints"
