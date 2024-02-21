fd83dbf5428d21439e650bb1bace5a1fce7c8574
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-02-21 22:16:15 +0000

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -94,7 +94,7 @@ module "eks_blueprints_addons" {
   cluster_version   = module.eks.cluster_version
   oidc_provider_arn = module.eks.oidc_provider_arn
 
-  # This is required to expose Istio Ingress Gateway
+  # This is required to expose Cilium Ingress
   enable_aws_load_balancer_controller = true
 
   tags = local.tags
