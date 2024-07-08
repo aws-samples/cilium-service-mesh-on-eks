5e095e1f7109adeb874b41c9296bb8ada22564aa
Dumlu Timuralp <34840364+dumlutimuralp@users.noreply.github.com>
2024-07-08 20:39:33 +0100

    Update versions.tf

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -15,11 +15,4 @@ terraform {
       version = ">= 2.20"
     }
   }
-
-  # ##  Used for end-to-end testing on project; update to suit your needs
-  # backend "s3" {
-  #   bucket = "terraform-ssp-github-actions-state"
-  #   region = "us-west-2"
-  #   key    = "e2e/istio/terraform.tfstate"
-  # }
 }
